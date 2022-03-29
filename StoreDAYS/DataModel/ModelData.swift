8e2cbd87480f92173b43744ab10c201c53a9d7f8
stephen-a-weber <pecuniary@gmail.com>
2022-03-29 17:27:19 -0400

    Starting branch on Github

---
+++ StoreDAYS/DataModel/ModelData.swift
@@ -0,0 +1,24 @@
+//
+//  ModelData.swift
+//  StoreDAYS
+//
+//  Created by stephen weber on 3/29/22.
+//
+
+import Foundation
+//import SwiftUI
+
+
+class ModelData : ObservableObject {
+    
+     struct typeInventory {
+                            var drugs : String
+                            var alchohol : String
+                            var eVapes : String
+        
+    }
+    
+ 
+    
+    
+}
