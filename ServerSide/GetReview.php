7d0628d3023229196bb4e51dfa23ea64dbaa749e
Andrew Malmstead <Malmsteadaa@gmail.com>
2022-03-31 14:56:14 -0700

    Update GetReview.php

--- ServerSide/GetReview.php
+++ ServerSide/GetReview.php
@@ -1,9 +1,5 @@
 <?php
 require_once "config.php";
-$sql = "SELECT * FROM User";
-$result = $_SERVER['dbconnection']->prepare($sql);
-$rows = $result->fetch_all(MYSQLI_ASSOC);
-echo json_encode($rows);
 if(isset($_GET['ID'])&isset($_GET['Body']) && isset($_GET['User_ID'])&& isset($_GET['Rate'])&& isset($_GET['Items_ID'])&& isset($_GET['Catagory_ID']) ){
     $ID= $_GET['ID'];
         $Body= $_GET['Body'];
@@ -14,4 +10,9 @@ if(isset($_GET['ID'])&isset($_GET['Body']) && isset($_GET['User_ID'])&& isset($_
     echo json_encode(new ReviewModel($ID,$Body,$User_ID,$Rate,$Items_ID,$Catagory_ID));
     
     }
+$sql = "SELECT * FROM User";
+$result = $_SERVER['dbconnection']->prepare($sql);
+$rows = $result->fetch_all(MYSQLI_ASSOC);
+echo json_encode($rows);
+
 ?>
