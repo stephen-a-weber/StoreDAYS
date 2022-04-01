93788541754466b62f7cb8a3a9382572d465007a
Andrew Malmstead <Malmsteadaa@gmail.com>
2022-04-01 04:40:16 -0700

    change to lamp

--- ServerSide/config.php
+++ ServerSide/config.php
@@ -6,8 +6,8 @@ $_SERVER["dbconnection"] = getConnection();
 function getConnection() {
     // CHANGE TO YOUR LOCAL DB CONFIGURATION //
     $hostname = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "user12";
+    $password = "123";
     $database = "StoreDAYS";
     $conn = mysqli_connect($hostname, $username, $password, $database);
     if(!$conn) die("Connection Failed: " . mysqli_connect_error());
