62b6321b8bcd9fe0d88811b968e63f82c3249e24
Andrew Malmstead <Malmsteadaa@gmail.com>
2022-04-04 01:52:10 -0700

    Update getItems.php

--- ServerSide/getItems.php
+++ ServerSide/getItems.php
@@ -7,27 +7,25 @@ require_once "ReviewModel.php";
     if(isset($_GET['ID'])){
       $ID = $_GET['ID'];
       $sql = "SELECT * FROM Items WHERE ID = ?";
-      $stmt = $_SERVER['dbconnection']->prepare($sql);
-      $stmt -> bind_param("i",$ID);
-      $stmt->execute();
+      $stmt = $_SERVER['dbconnection']->prepare($sql)or die("Couldn't prepare".htmlspecialchars($stmt->error));;
+      $stmt -> bind_param("i",$ID)or die("Couldn't bind".htmlspecialchars($stmt->error));;
+      $stmt->execute()or die("Couldn't excute".htmlspecialchars($stmt->error));;
     $rows=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
       echo json_encode($rows);
     }
     else if(isset($_GET['Catagory_ID'])){
       $ID = $_GET['Catagory_ID'];
       $sql = "SELECT * FROM Items WHERE Catagory_ID = ?";
-      $stmt = $_SERVER['dbconnection']->prepare($sql) or die("Couldn't prepare".htmlspecialchars($stmt->error));;
+      $stmt = $_SERVER['dbconnection']->prepare($sql)or die("Couldn't prepare".htmlspecialchars($stmt->error));
       $stmt -> bind_param("i",$ID) or die("Couldn't bind".htmlspecialchars($stmt->error));
-      $stmt->execute()or die("Couldn't Execute".htmlspecialchars($stmt->error));
+      $stmt->execute()or die("Couldn't excute".htmlspecialchars($stmt->error));
     $rows=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
       echo json_encode($rows);
     }else{
-      $sql = "SELECT * FROM Items WHERE Availability= ?";
-      $stmt = $_SERVER['dbconnection']->prepare($sql) or die("Couldn't prepare".htmlspecialchars($stmt->error));;
-      $stmt -> bind_param("i",1) or die("Couldn't bind".htmlspecialchars($stmt->error));
-      $stmt->execute()or die("Couldn't Execute".htmlspecialchars($stmt->error));
-      $rows=$stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-      echo json_encode($rows);
+$sql = "SELECT * FROM Items WHERE Availability=1";
+$result = $_SERVER['dbconnection']->query($sql, MYSQLI_USE_RESULT) or die("Couldn't excute".htmlspecialchars($stmt->error));;
+$rows = $result->fetch_all(MYSQLI_ASSOC);
+echo json_encode($rows);
     }
 
 
