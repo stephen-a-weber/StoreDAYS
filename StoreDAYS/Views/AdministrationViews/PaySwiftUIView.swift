688499c34d826a69084dc2bdf5d2639dfef07422
Young Jin <crecre4x@gmail.com>
2022-04-04 12:10:00 -0600

    Connected calls to PaySwiftUIView.

--- StoreDAYS/Views/AdministrationViews/PaySwiftUIView.swift
+++ StoreDAYS/Views/AdministrationViews/PaySwiftUIView.swift
@@ -5,22 +5,70 @@
 
 //
 
-import SwiftUI
+      //
+      //  PaySwiftUIView.swift
+      //  StoreDAYS
+      //
 
-struct PaySwiftUIView: View {
-    @State var user: String = ""
-    var body: some View {
-        
-        VStack {
-            Text( " Pay")
-            Text(user)
-        }
-       
-    }
-}
+      //
 
-struct PaySwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        PaySwiftUIView(user: "ddt")
-    }
-}
+      import SwiftUI
+
+      struct PaySwiftUIView: View {
+            @State var user: String = ""
+
+
+          var body: some View {
+                NavigationView {
+
+              VStack{
+      //            CardSetupButton(action: {})
+      //                .frame(minWidth: 200, maxWidth: 400)
+      //                .frame(height: 45)
+      //                .frame(maxWidth: .infinity)
+      //                       .padding()
+                    PaymentButton(action: {})
+                        .frame(minWidth: 400, maxWidth: 400)
+                        .frame(height: 45)
+                        .frame(maxWidth: .infinity)
+                              .padding()
+      //              NavigationLink(isActive: true) { T2View()}
+                    NavigationLink {
+                          T2View()
+                            .frame(height: 350)
+                    }
+                label: {
+                        Text("Show me current spot of my pets?")
+                            .foregroundColor(Color.black)
+                    }
+      //              Spacer()
+      //              Text(user)
+         }
+                }
+          }
+      }
+
+      struct PaySwiftUIView_Previews: PreviewProvider {
+          static var previews: some View {
+              PaySwiftUIView(user: "ddt")
+          }
+      }
+
+      //struct PaySwiftUIView: View {
+      //    @State var user: String = ""
+      //    var body: some View {
+      //
+      //        VStack {
+      //            Text( " Pay")
+      //            Text(user)
+      //        }
+      //
+      //    }
+      //}
+      //
+      //struct PaySwiftUIView_Previews: PreviewProvider {
+      //    static var previews: some View {
+      //        PaySwiftUIView(user: "ddt")
+      //    }
+      //}
+
