f02b030239318e852980c8b3f8190a6f01dc9b28
Andrew Malmstead <Malmsteadaa@gmail.com>
2022-04-11 14:59:23 -0700

    fix querry

--- ServerSide/getItems.php
+++ ServerSide/getItems.php
@@ -22,7 +22,7 @@ require_once "config.php";
       echo json_encode($rows);
     }else if(!empty($_POST['Invoice_ID'])){
       $ID = $_GET['Invoice_ID'];
-      $sql = "SELECT * FROM Items WHERE Invoice_ID = ?";
+      $sql = "SELECT Item_ID from Orders_Details WHERE Orders_ID = (SELECT ID From Orders WHERE Invoice_ID = ?)      ";
       $stmt = $_SERVER['dbconnection']->prepare($sql)or die("Couldn't prepare".htmlspecialchars($stmt->error));
       $stmt -> bind_param("i",$ID) or die("Couldn't bind".htmlspecialchars($stmt->error));
       $stmt->execute()or die("Couldn't excute".htmlspecialchars($stmt->error));
