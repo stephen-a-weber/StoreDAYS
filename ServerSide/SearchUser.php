374fe321901dbf366fc7db45892226b403dbefeb
Andrew Malmstead <Malmsteadaa@gmail.com>
2022-04-15 14:19:40 -0700

    Adding a new End

---
+++ ServerSide/SearchUser.php
@@ -0,0 +1,18 @@
+<?php
+require_once "config.php";
+
+if (!empty($_GET['Email'])) {
+    $Email = $_GET["Email"];
+    $sql = "SELECT ID FROM User WHERE Email=?";
+    $stmt = $_SERVER['dbconnection']->prepare($sql) or die("Couldn't prepare" . htmlspecialchars($stmt->error));
+    $stmt->bind_param("s", $email) or die("Couldn't bind" . htmlspecialchars($stmt->error));
+    $stmt->execute() or die("Couldn't excute" . htmlspecialchars($stmt->error));
+    $rows = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    if (empty($rows))
+        echo json_encode(0);
+    else
+        echo json_encode($rows[0]);
+} else {
+    die("Post was not met");
+}
+?>
