4ee4f3467264757a1b3dec3073420b1be27880bd
Andrew Malmstead <Malmsteadaa@gmail.com>
2022-04-18 07:19:40 -0700

    Update SearchUser.php

--- ServerSide/SearchUser.php
+++ ServerSide/SearchUser.php
@@ -5,7 +5,7 @@ if (!empty($_GET['Email'])) {
     $Email = $_GET["Email"];
     $sql = "SELECT ID FROM User WHERE Email=?";
     $stmt = $_SERVER['dbconnection']->prepare($sql) or die("Couldn't prepare" . htmlspecialchars($stmt->error));
-    $stmt->bind_param("s", $email) or die("Couldn't bind" . htmlspecialchars($stmt->error));
+    $stmt->bind_param("s", $Email) or die("Couldn't bind" . htmlspecialchars($stmt->error));
     $stmt->execute() or die("Couldn't excute" . htmlspecialchars($stmt->error));
     $rows = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
