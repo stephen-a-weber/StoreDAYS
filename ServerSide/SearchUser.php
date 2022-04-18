fda5135b212ae208e06e5d7369db324be844fa22
Andrew Malmstead <Malmsteadaa@gmail.com>
2022-04-18 07:26:58 -0700

    Update SearchUser.php

--- ServerSide/SearchUser.php
+++ ServerSide/SearchUser.php
@@ -8,8 +8,9 @@ if (!empty($_GET['Email'])) {
     $stmt->bind_param("s", $Email) or die("Couldn't bind" . htmlspecialchars($stmt->error));
     $stmt->execute() or die("Couldn't excute" . htmlspecialchars($stmt->error));
     $rows = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $Empty['ID']=0;
     if (empty($rows))
-        echo json_encode(0);
+        echo json_encode($Empty);
     else
         echo json_encode($rows[0]);
 } else {
